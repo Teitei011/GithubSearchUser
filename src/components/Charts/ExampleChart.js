a603d61eb2b731b01840729d4d67df921d581b12
Teitei011 <stefantleal14@gmail.com>
2021-05-28 21:00:31 -0300

    More modifications

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-const ExampleChart = () => {
-  return <div>chart</div>;
-};
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
-export default ExampleChart;
+
+const ChartComponent = ({data}) =>{
+  const chartConfigs = {
+    type: 'column2d',
+    width: 600,
+    height: 400,
+    dataFormat: 'json',
+    dataSource:data,
+  };
+
+  return <ReactFC {...chartConfigs} />;
+
+}
+
+
+export default ChartComponent;
